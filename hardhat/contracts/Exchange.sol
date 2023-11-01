62c76851954553e757286ea020bb73b440d766e8
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2023-11-01 18:17:11 +0530

    Updatd

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -4,5 +4,58 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Exchange is ERC20 {
-    // Future code goes here
-}
+    address public tokenAddress;
+
+    // Exchange is inheriting ERC20, because our exchange itself is an ERC-20 contract
+    // as it is responsible for minting and issuing LP Tokens
+    constructor(address token) ERC20("ETH TOKEN LP Token", "lpETHTOKEN"){
+        require(token != address(0), "Token address passed is a null address");
+        tokenAddress = token;
+    }
+
+    // getReserve returns the balance of 'token' held by 'this' contract
+    function getReserve() public view returns (uint256) {
+        return ERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    // addLiquidity allows users to add liquidity to the exchange 
+    function addLiquidity(uint256 amountOfToken) public payable returns (uint256) {
+        uint256 lpTokensToMint;
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 tokenReserveBalance = getReserve();
+
+        ERC20 token = ERC20(tokenAddress);
+
+        // If the reserve is empty, take any user supplied value for initial liquidity
+        if(tokenReserveBalance == 0){
+            // Transfer the token from the user to the exchange
+            token.transferFrom(msg.sender, address(this),amountOfToken);
+
+            // lpTokensToMint = ethReserveBalance = msg.value
+            lpTokensToMint = ethReserveBalance;
+
+            // Mint LP tokens to the user
+            _mint(msg.sender, lpTokensToMint);
+
+            return lpTokensToMint;
+        }
+
+        // If the reserve is not empty, calculate the amount of LP Tokens to be minted
+        uint256 ethReservePriorToFunctionCall = ethReserveBalance - msg.value;
+        uint256 minTokenAmountRequired = (msg.value * tokenReserveBalance)/ethReservePriorToFunctionCall;
+
+        require(
+            amountOfToken >= minTokenAmountRequired, "Insufficient amount of tokens provided"
+        );
+
+        // Transfer the token from the user to the exchange
+        token.transferFrom(msg.sender, address(this),minTokenAmountRequired);
+
+        // Calculate the amount of LP tokens to be minted
+        lpTokensToMint = (totalSupply()*msg.value)/ethReservePriorToFunctionCall;
+
+        // Mint LP tokens to the user
+        _mint(msg.sender, lpTokensToMint);
+        
+        return lpTokensToMint;
+    }
