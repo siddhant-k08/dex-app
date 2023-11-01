4ba263a221247eb2cd4a1fb37349ab8b184fa144
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2023-11-01 18:55:55 +0530

    Deployment Script

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,33 +1,41 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve,ms));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
+async function main(){
+  // Deploy the Token contract
+  const tokenContract = await hre.ethers.deployContract("Token");
+  await tokenContract.waitForDeployment();
+  console.log("Token deployed to:",tokenContract.target);
+
+  // Deploy the Exchange Contact
+  const exchangeContract = await hre.ethers.deployContract("Exchange",[
+    tokenContract.target,
+  ]);
+  await exchangeContract.waitForDeployment();
+  console.log("Exchange deployed to:",exchangeContract.target);
+
+  // Wait for 30 seconds to let Etherscan catch up on contract deployments
+  await sleep(30 * 1000);
+
+  // Verify the contracts on Etherscan
+  await hre.run("verify:verify", {
+    address: tokenContract.target,
+    constructorArguments: [],
+    contract: "contracts/Token.sol:Token",
+  });
+
+  await hre.run("verify:verify",{
+    address: exchangeContract.target,
+    constructorArguments: [tokenContract.target],
+  });
+}
+
+// We recomment this pattern to be able to use async/await everywhere
+// and properly handle errors
+main().catch((error)=>{
+  console.log(error);
   process.exitCode = 1;
 });
